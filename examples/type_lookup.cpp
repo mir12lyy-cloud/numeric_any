af574c0849798e744f1206cb1c7f7c9d8398908b
casyyy <huangjinyangyang@hotmail.com>
2026-08-27 22:30:19 +0800

    fix: removed the remnants of old functions in the sample code

--- examples/type_lookup.cpp
+++ examples/type_lookup.cpp
@@ -8,10 +8,5 @@ int main() {
     numeric_any u = 12;
     std::println("{}", v.is_same_type<int>());
     std::println("{}", v.is_same_type<long>());
-    std::println("{}", v.can_safe_convert_to<long long>());
-    std::println("{}", u.can_safe_convert_to<unsigned long long>());
-    std::println("{}", v.can_safe_convert_to<unsigned>());
-    std::println("{}", v.can_safe_convert_to<short>());
-    std::println("{}", v.can_safe_convert_to<float>());
     return 0;
 }
